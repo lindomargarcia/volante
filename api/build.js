e45bff0a5b0ed630a46c1e3c7f3fbf3ef77f6fdd
lindomargarcia <garcialindomar2@gmail.com>
2024-10-16 16:10:21 -0300

    new: build file - API

---
+++ api/build.js
@@ -0,0 +1,11 @@
+// build.js
+import esbuild from 'esbuild'
+
+esbuild.build({
+  entryPoints: ['./index.js'], // Arquivo de entrada principal
+  bundle: true, // Empacota todas as dependências em um único arquivo
+  minify: true, // Minimiza o código para produção
+  platform: 'node', // Define como plataforma Node.js
+  target: 'node14', // Define a versão-alvo do Node.js
+  outfile: 'dist/index.js', // Arquivo de saída
+}).catch(() => process.exit(1));
