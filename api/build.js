df22ddeb1138d6193df470672730ed972f0f032b
lindomargarcia <garcialindomar2@gmail.com>
2024-10-20 14:36:35 -0300

    update: API config

--- api/build.js
+++ api/build.js
@@ -6,6 +6,6 @@ esbuild.build({
   bundle: true, // Empacota todas as dependências em um único arquivo
   minify: true, // Minimiza o código para produção
   platform: 'node', // Define como plataforma Node.js
-  target: 'node14', // Define a versão-alvo do Node.js
+  target: 'node18', // Define a versão-alvo do Node.js
   outfile: 'dist/index.js', // Arquivo de saída
 }).catch(() => process.exit(1));
--- api/config/database.js
+++ api/config/database.js
@@ -1,8 +1,10 @@
 import { Sequelize } from "sequelize";
+import sqlite3 from "sqlite3";
 
 const db = new Sequelize({
   storage: './database.db',
   dialect: 'sqlite',
+  dialectModule: sqlite3
 });
 
 export default db
