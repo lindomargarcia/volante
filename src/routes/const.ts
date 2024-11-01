1509afcb68e4f5ceae61a3ede380aaaa5857fc8c
lindomargarcia <garcialindomar2@gmail.com>
2024-11-01 12:33:47 -0300

    new: Backend URL

--- src/routes/const.ts
+++ src/routes/const.ts
@@ -1,1 +1,1 @@
-export const BASE_URL = import.meta.env.DEV ? "http://localhost:3000/" : "https://api-volante.fly.dev/"
+export const BASE_URL = import.meta.env.DEV ? "http://localhost:3000/" : "https://volante-backend.fly.dev/"
